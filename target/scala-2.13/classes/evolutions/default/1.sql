46014e71503c67590617b4236ab1c5874fb7773a
Hong Yew Lam (Kelvin) <hylam98tom@gmail.com>
2022-06-09 12:42:17 +0800

    db created

--- target/scala-2.13/classes/evolutions/default/1.sql
+++ target/scala-2.13/classes/evolutions/default/1.sql
@@ -24,13 +24,36 @@ create table `maintenance_item` (
 create table `work_order` (
   `id` bigint(20) not null auto_increment,
   `person_in_charge` varchar(100) not null,
+  `technician_id` bigint(20) not null,
   `maintenance_name` varchar(100) not null,
-  `maintenance_date` date not null,
+  `maintenance_date` datetime not null,
   `status` varchar(100) not null,
   primary key (`id`)
 );
+
+create table `technician` (
+  `id` bigint(20) not null auto_increment,
+  `name` varchar(100) not null,
+  `contact_number` varchar(100) not null,
+  primary key (`id`)
+);
+
+create view `work_order_view` as
+select
+wo.id,
+wo.person_in_charge,
+wo.maintenance_name,
+wo.maintenance_date,
+wo.status,
+wo.technician_id,
+t.name,
+t.contact_number
+from work_order wo
+inner join technician t on (t.id = wo.technician_id);
 
 # --- !Downs
 drop table `users`;
 drop table `maintenance_item`;
 drop table `work_order`;
+drop table `technician`;
+drop view `work_order_view`;
